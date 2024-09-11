e13ec7e8bddf7092ae96c31d93f78774629da921
beadidd <57589034+FlameProfi@users.noreply.github.com>
2024-09-11 17:15:27 +0300

    inital commit

---
+++ reactapp1.client/src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home.tsx';
+import Login from './Pages/Login.tsx';
+import Register from './Pages/Register.tsx';
+import styles from './styles/index.scss';
+import Modal from './Components/modalOrder/index.tsx';
+
+function App() {
+    return (
+        //<Modal active={true} setActive={true}></Modal>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/card/:productId" element={<Modal active={true} setActive={true}/>} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
+
+}
+export default App;
---
+++ reactapp1.client/src/Components/modalOrder/index.tsx
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from "react";
+import './assets/style.scss'
+import { useParams } from "react-router-dom";
+import testImg from "./assets/test_logo.png";
+import star from "./assets/star.svg";
+interface ICard {
+    id: number;
+    name: string;
+    size: string;
+    canvas: string;
+    TODO1: string;
+    TODO2: string;
+}
+
+const Modal = ({active, setActive} : {active: any, setActive: any}) => {
+    const { productId } = useParams();
+    const [card, setCard] = useState<ICard>();
+    const apiUrl = `http://localhost:7209/cardId=${productId}`;
+
+    useEffect(() => {
+        getCard();
+    }, []);
+
+
+
+
+    return (
+        <div className={active ? "modal active" : "modal"} onClick={()=> setActive(false)}>
+            <div className="modal__content" onClick={e => e.stopPropagation()}>
+                <div className="top">
+                    <div className="left">
+                        <h1>{card?.name}</h1>
+                        <div className="description">
+                            <div className="description__item"> 
+                            <p>Размер(шXвXд)</p><span>{card?.size}</span>
+                            </div>
+                            <div className="description__item"> 
+                            <p>Холст</p><span>{card?.canvas}</span>
+                            </div>
+                            <div className="description__item"> 
+                            <p>Комплектация</p><span>xkxskxkxk</span>
+                            </div>
+                            <div className="description__item"> 
+                            <p>Масса товара с упаковкой</p><span>xkxskxkxk</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="right">
+                        <img src={testImg} alt="" />
+                    </div>
+                </div>
+                <button className="toSellerPage">Перейти на страницу продавца</button>
+                <div className="reviews">
+                    <h1>Отзывы</h1>
+                    <div className="reviews__items">
+                        <div className="reviews__item"> 
+                            <div className="name__stars"> 
+                            <h1>Алек</h1>
+                            <div className="stars"> 
+                            <img src={star} alt="" />
+                            <img src={star} alt="" />
+                            <img src={star} alt="" />
+                            <img src={star} alt="" />
+                            <img src={star} alt="" />
+                            </div>
+                            </div>
+                            <p>Ужасная доставка, все не по срокам, не советую!!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
+
+
+
+    async function getCard() {
+        setCard({id: 1, name: "Картина", size: "100x100x100", canvas: "Бумага", TODO1: "sdllsd", TODO2: "lfdlfdlfd"});
+        fetch(apiUrl)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Данные не найдены');
+          }
+          return response.json();
+        })
+        .then((data: ICard) => setCard(data))
+        .catch((error) => {
+          if (error.message === 'Данные не найдены') {
+            // Обработка ошибки 404
+            console.error('Ошибка 404: Данные не найдены');
+          } else {
+            console.error('Произошла ошибка при загрузке данных:', error);
+          }
+        });
+    }
+};
+export default Modal;
