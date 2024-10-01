9713a4f6260d489db6ea08b877feeec89279d4fb
beadidd <57589034+FlameProfi@users.noreply.github.com>
2024-10-01 14:27:48 +0000

    upd header commit

--- reactapp1.client/src/Components/Header/index.tsx
+++ reactapp1.client/src/Components/Header/index.tsx
@@ -1,32 +1,36 @@
 import React, { useEffect, useState } from "react";
-import logo from "./assets/stena.png";
+import logo from "./assets/logo.svg";
+import search_logo from "./assets/search_logo.png";
 import './assets/style.scss'
-
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
+    const navigate = useNavigate();
+
+    const handleSearch = () => {
+        alert("кнопка")
+    }
+    const handleLogin = () => {
+        navigate("/login");
+    }
 
     return (
         <div className="header">
-            <div className="burger">
-                <div className="item" />
-                <div className="item" />
-                <div className="item" />
+            <img src={logo} alt="" />
+            <div className="header__menu">
+                <p>ПРОЕКТЫ</p>
+                <p>АВТОРЫ</p>
+                <p>ВЕБИНАРЫ</p>
+                <p>ВЫСТАВКИ В СМОЛЕНСКЕ</p>
+                <p>ОНЛАЙН СОБЫТИЯ</p>
             </div>
-
-            <div className="center">
-                <div className="logo">
-                <img src={logo} alt="" />
-                <h1>ArtConnect Smolensk</h1></div>
-                <div className="new__items">
-                    <p>НОВИНКА</p>
-                    <p>НОВИНКА</p>
-                    <p>НОВИНКА</p>
-                    <p>НОВИНКА</p>
-                    <p>НОВИНКА</p>
-                    <p>НОВИНКА</p>
-                </div>
+            <div className="search__login"> 
+            <div className="header__search">
+                <img src={search_logo} alt="" onClick={handleSearch}/>
+                <input type="text" placeholder="Поиск" />
             </div>
-
+            <button className="header__login" onClick={handleLogin}>Войти</button>
+            </div>
         </div>
     )
 
