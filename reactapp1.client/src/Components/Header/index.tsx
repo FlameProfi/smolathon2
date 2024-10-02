ba4068094882dcc2eaef69416a13fba755dfaf0e
beadidd <57589034+FlameProfi@users.noreply.github.com>
2024-10-02 13:29:10 +0000

    added search system commit

--- reactapp1.client/src/Components/Header/index.tsx
+++ reactapp1.client/src/Components/Header/index.tsx
@@ -1,36 +1,73 @@
 import React, { useEffect, useState } from "react";
 import logo from "./assets/logo.svg";
-import search_logo from "./assets/search_logo.png";
+import searchLogo from "./assets/search.svg";
+import profile from "./assets/profile.svg";
+import message from "./assets/message.svg";
 import './assets/style.scss'
 import { useNavigate } from "react-router-dom";
 
-export const Header = (props: { authorized: boolean }) => {
+export const Header = (_props: { authorized: boolean }) => {
     const navigate = useNavigate();
+    const [search, setSearch] = useState<boolean>(false);
+    const [inputSearch, setInputSearch] = useState<string>("");
 
     const handleSearch = () => {
-        alert("кнопка")
+        if(search){
+            setSearch(false)
+            // ЛОГИКА ПОИСКА
+            console.log(inputSearch)
+        }else{
+            setSearch(true)
+        }
     }
+    const inputCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        if (name === "inputSearch") setInputSearch(value);
+    };
     const handleLogin = () => {
         navigate("/login");
     }
 
     return (
-        <div className="header">
-            <img src={logo} alt="" />
-            <div className="header__menu">
-                <p>ПРОЕКТЫ</p>
+        <div className="header" style={{marginLeft: "30px"}}>
+            <div className="logo__">
+            <img src={logo} alt="" style={{marginRight: "20px"}} />
+            <div className="logo__name">
+                <h1>ARTCONNECT</h1>
+                <div className="logo__name__block">
+                    <div className="block" />
+                    <p>SMOLENSK</p>
+                    <div className="block" />
+                </div>
+            </div>
+            </div>
+            <div className={search ? "header__menu" : "header__menu active"} style={{marginLeft: "90px"}}>
                 <p>АВТОРЫ</p>
+                <p>ПРОЕКТЫ</p>
                 <p>ВЕБИНАРЫ</p>
                 <p>ВЫСТАВКИ В СМОЛЕНСКЕ</p>
                 <p>ОНЛАЙН СОБЫТИЯ</p>
             </div>
-            <div className="search__login"> 
+            <input type="text" 
+            className={search ? "input_test active" : "input_test"}
+            style={{marginLeft: "90px"}} 
+            name="inputSearch"
+            value={inputSearch}
+            onChange={inputCheck}
+            placeholder="Поиск"/>
+            <div className="icons">
+                <img src={searchLogo} alt="" onClick={handleSearch}/>
+                <img src={message} alt="" />
+                <img src={profile} alt="" />
+
+            </div>
+            {/* <div className="search__login"> 
             <div className="header__search">
                 <img src={search_logo} alt="" onClick={handleSearch}/>
                 <input type="text" placeholder="Поиск" />
             </div>
             <button className="header__login" onClick={handleLogin}>Войти</button>
-            </div>
+            </div> */}
         </div>
     )
 
